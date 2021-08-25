776bf7f3d579a31f0e97c396f40ffea13641b60c
mtmahe <mtmahe@gmail.com>
2021-08-25 15:17:41 -0300

    removed vanilla js for react in .html

---
+++ network/static/network/maybe.js
@@ -0,0 +1,58 @@
+Class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write your new post here.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A post was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          New Post:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    );
+  }
+}
+
+
+
+ReactDOM.render(
+  <PostForm />,
+  document.getElementById('new-post')
+);
+
+
+function PostsView(props) {
+  const posts = props.posts;
+  const listItems = posts.map((post) =>
+    <li className="section-container" >{post}</li>
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  )
+}
+
+const posts = ['first post', 'second post', 'third post'];
+ReactDOM.render (
+  <PostsView posts={posts}/>,
+  document.getElementById('posts-list')
+);
